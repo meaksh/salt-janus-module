16aa3c374830fbf3ea04c111c9e3a24c83f24f4c
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2016-06-27 10:16:40 +0200

    Some cleanup and refactoring

--- srv/salt/_modules/janus.py
+++ srv/salt/_modules/janus.py
@@ -27,10 +27,16 @@ def __virtual__():
 
 class JanusSession(object):
     def __init__(self, *args, **kwargs):
-        self._janus_proto = kwargs.get("janus_proto", "http")
-        self._janus_uri = kwargs.get("janus_hostname", "localhost")
-        self._janus_port = kwargs.get("janus_port", "8088")
-        self._janus_base = kwargs.get("janus_base", "janus")
+        self.set_config()
+
+    def set_config(self, opts=None):
+        if not opts:
+            opts = dict()
+
+        self._janus_proto = opts.get("janus_proto", "http")
+        self._janus_uri = opts.get("janus_hostname", "localhost")
+        self._janus_port = opts.get("janus_port", "8088")
+        self._janus_base = opts.get("janus_base", "janus")
 
         self._janus_api_root = "{0}://{1}:{2}/{3}".format(self._janus_proto,
                                                           self._janus_uri,
@@ -40,7 +46,8 @@ class JanusSession(object):
     def _random_token(self):
         return "%008x" % random.getrandbits(64)
 
-    def _create_instance(self):
+    def _create_instance(self, config):
+        self.set_config(config)
         data = {"janus": "create", "transaction": self._random_token()}
         resp = requests.post(self._janus_api_root, json.dumps(data))
         return resp.json().get("data")
@@ -54,24 +61,54 @@ class JanusSession(object):
     def _message_request(self, session_id, plugin_id, message):
         plugin_uri = self._janus_api_root + "/" + str(session_id) + "/" + str(plugin_id)
         data = {"janus": "message", "body": message, "transaction": self._random_token()}
-        log.warning("--> FINAL: %s" % plugin_uri)
-        log.warning(data)
         resp = requests.post(plugin_uri, json.dumps(data))
         return resp.json()
 
 
 janus = JanusSession()
 
-def list_rooms(config=None):
-    instance = janus._create_instance()
-    plugin = janus._attach_plugin(instance['id'], "janus.plugin.videoroom")
-    message = {"request": "list"}
-    resp = janus._message_request(instance['id'], plugin['id'], message)
-    return resp
+def list_videorooms(config=None):
+    '''
+    List the current list of videorooms availables in Janus service instance
 
-def create_room(config=None):
-    instance = janus._create_instance()
-    plugin = janus._attach_plugin(instance['id'], "janus.plugin.videoroom")
-    message = {"request": "create", "id": "11234", "description": "WOOOS AHORA SI", "bitrate": 64, "publishers": 10}
-    resp = janus._message_request(instance['id'], plugin['id'], message)
-    return resp
+    CLI example:
+
+    .. code-block:: bash
+
+        salt '*' janus.list_videorooms
+    '''
+    try:
+        instance = janus._create_instance(config)
+        plugin = janus._attach_plugin(instance['id'], "janus.plugin.videoroom")
+        message = {"request": "list"}
+        resp = janus._message_request(instance['id'], plugin['id'], message)
+        return resp
+    except requests.exceptions.ConnectionError as exc:
+        raise CommandExecutionError(
+            'Error encountered while listing Janus videorooms: {0}'
+            .format(exc)
+        )
+
+
+def create_videoroom(name, publishers=20, bitrate=64, id=None, config=None):
+    '''
+    Create a new videoroom in Janus service instance
+
+    CLI example:
+
+    .. code-block:: bash
+
+        salt '*' janus.create_videoroom "my tests videoroom"
+        salt '*' janus.create_videoroom testvideoroom bitrate=128 publishers=50
+    '''
+    try:
+        instance = janus._create_instance(config)
+        plugin = janus._attach_plugin(instance['id'], "janus.plugin.videoroom")
+        message = {"request": "create", "id": id, "description": name, "bitrate": bitrate, "publishers": publishers}
+        resp = janus._message_request(instance['id'], plugin['id'], message)
+        return resp
+    except requests.exceptions.ConnectionError as exc:
+        raise CommandExecutionError(
+            'Error encountered while listing Janus videorooms: {0}'
+            .format(exc)
+        )
